87c708ae6dca921dbc696364a4dd2c820b7aa2e4
Icydreamer <1091939105@qq.com>
2023-10-16 10:47:10 +0800

    lab1：Event

---
+++ lab1/EventHandle/EventHandle/FireAlarm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHandle
+{
+    public class FireAlarm
+    {
+        public delegate void FireEventHandler(object sender, FireEventArgs fe);
+        public event FireEventHandler FireEvent;
+        public void ActivateFireAlarm(string room, int ferocity)
+        {
+            FireEventArgs fireArgs = new FireEventArgs(room, ferocity);
+            FireEvent(this, fireArgs);
+        }
+    }
+}
